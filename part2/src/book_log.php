03d36a5142ef1f73cff02dc62f345e07064dc48e
crypt0box <pray.happy76@gmail.com>
2021-01-03 12:26:56 +0900

    ADD: initial commit

--- part2/src/book_log.php
+++ part2/src/book_log.php
@@ -1,10 +1,46 @@
 <?php
 
-$title = '';
-$author = '';
-$status = '';
-$rate = '';
-$review = '';
+function createBook()
+{
+    echo '読書ログを登録してください' . PHP_EOL;
+    echo '書籍名：';
+    $title = trim(fgets(STDIN));
+
+    echo '著者名：';
+    $author = trim(fgets(STDIN));
+
+    echo '読書状況（未読，読んでる，読了）：';
+    $status = trim(fgets(STDIN));
+
+    echo '評価（５点満点の整数）：';
+    $rate = trim(fgets(STDIN));
+
+    echo '感想：';
+    $review = trim(fgets(STDIN));
+
+    echo '登録が完了しました' . PHP_EOL . PHP_EOL;
+
+    return [
+      'title' => $title,
+      'author' => $author,
+      'status' => $status,
+      'rate' => $rate,
+      'review' => $review,
+    ];
+}
+
+function displayBooks($books)
+{
+    echo '読書ログを表示します' . PHP_EOL;
+    foreach ($books as $book) {
+        echo '書籍名：' . $book['title'] . PHP_EOL;
+        echo '著者名：' . $book['author'] . PHP_EOL;
+        echo '読書状況：' . $book['status'] . PHP_EOL;
+        echo '評価：' . $book['rate'] . PHP_EOL;
+        echo '感想：' . $book['review'] . PHP_EOL;
+        echo '------------' . PHP_EOL;
+    }
+}
 
 $books = [];
 
@@ -16,46 +52,11 @@ while (true) {
     $num = trim(fgets(STDIN));
     
     if ($num === '1') {
-        echo '読書ログを登録してください' . PHP_EOL;
-        echo '書籍名：';
-        $title = trim(fgets(STDIN));
-      
-        echo '著者名：';
-        $author = trim(fgets(STDIN));
-      
-        echo '読書状況（未読，読んでる，読了）：';
-        $status = trim(fgets(STDIN));
-      
-        echo '評価（５点満点の整数）：';
-        $rate = trim(fgets(STDIN));
-      
-        echo '感想：';
-        $review = trim(fgets(STDIN));
-
-        $books[] = [
-          'title' => $title,
-          'author' => $author,
-          'status' => $status,
-          'rate' => $rate,
-          'review' => $review,
-        ];
-      
-        echo '登録が完了しました' . PHP_EOL . PHP_EOL;
+        $books[] = createBook();
     } elseif ($num === '2') {
-        echo '読書ログを表示します' . PHP_EOL;
-
-        foreach ($books as $book) {
-            echo '書籍名：' . $book['title'] . PHP_EOL;
-            echo '著者名：' . $book['author'] . PHP_EOL;
-            echo '読書状況：' . $book['status'] . PHP_EOL;
-            echo '評価：' . $book['rate'] . PHP_EOL;
-            echo '感想：' . $book['review'] . PHP_EOL;
-            echo '------------' . PHP_EOL;
-        }
+        displayBooks($books);
     } elseif ($num === '9') {
         // アプリケーションを終了する
         break;
     }
 }
-
-var_export($books);
