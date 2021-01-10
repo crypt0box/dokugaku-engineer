fc0488db7b594cdcccab217ef896a3a47d6e0a3b
crypt0box <pray.happy76@gmail.com>
2021-01-10 15:23:16 +0900

    ADD: initial commit

--- part2/src/companies/new.php
+++ part2/src/companies/new.php
@@ -12,6 +12,22 @@
 </head>
 
 <body>
-    <p>aaa</p>
+    <h1>会社情報の登録</h1>
+    <form action="" method="POST">
+        <div>
+            <label for="name">会社名</label>
+            <input type="text" id="name" name="name">
+        </div>
+        <div>
+            <label for="establishment_date">設立日</label>
+            <input type="date" name="establishment_date"
+            id="establishment_date">
+        </div>
+        <div>
+            <label for="founder">代表者</label>
+            <input type="text" name="founder" id="founder">
+        </div>
+        <button type="submit">登録する</button>
+    </form>
 </body>
 </html>
