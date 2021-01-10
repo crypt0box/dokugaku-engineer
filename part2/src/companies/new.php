e023fb648b34f7172510bcc415ee5f2352767f46
crypt0box <pray.happy76@gmail.com>
2021-01-10 14:38:43 +0900

    ADD: initial commit

---
+++ part2/src/companies/new.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!DOCKTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,
+    initial-scale=1">
+
+    <title>会社情報の登録</title>
+</head>
+
+<body>
+    <p>aaa</p>
+</body>
+</html>
