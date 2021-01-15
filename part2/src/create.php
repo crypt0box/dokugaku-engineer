dd0bd1e5f0cc70940c61121c7c0ef7e65b497b2f
crypt0box <pray.happy76@gmail.com>
2021-01-15 20:50:34 +0900

    ADD: initial commit

--- part2/src/create.php
+++ part2/src/create.php
@@ -27,7 +27,52 @@ function createReview($link, $review)
     }
 }
 
+function validate($review)
+{
+    $errors = [];
+
+    // タイトル
+    if (!strlen($review['title'])) {
+        $errors['title'] = 'タイトルを入力してください';
+    } elseif (strlen($review['title']) > 255) {
+        $errors['title'] = 'タイトルは255文字以内で入力してください';
+    }
+
+    // 著者名
+    if (!strlen($review['author'])) {
+        $errors['author'] = '著者名を入力してください';
+    } elseif (strlen($review['author']) > 50) {
+        $errors['author'] = '著者名は50文字以内で入力してください';
+    }
+
+    // 読書状況
+    if (!strlen($review['status'])) {
+        $errors['status'] = '読書状況を選択してください';
+    }
+
+    // 評価
+    if ($review['rate'] < 1 || $review['rate'] > 5) {
+        $errors['rate'] = '評価は1~5の整数を入力してください';
+    }
+
+    // 感想
+    if (!strlen($review['review'])) {
+        $errors['review'] = '感想を入力してください';
+    } elseif (strlen($review['review']) > 1024) {
+        $errors['review'] = '感想は1024文字以内で入力してください';
+    }
+
+    return $errors;
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // 読書状況のラジオボタンにチェックがされていない時に登録ボタンを押しても、エラーがでないようにする処理
+    $status = '';
+    if (array_key_exists('status', $_POST)) {
+        $status = $_POST['status'];
+    }
+
     $review = [
         'title' => $_POST['title'],
         'author' => $_POST['author'],
@@ -36,9 +81,71 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'review' => $_POST['review']
     ];
 
-    $link = dbConnect();
-    createReview($link, $review);
-    mysqli_close($link);
+    $errors = validate($review);
+
+    if (!count($errors)) {
+        $link = dbConnect();
+        createReview($link, $review);
+        mysqli_close($link);
+        header("Location: index.php");
+    }
 }
 
-header("Location: index.php");
+
+?>
+<!DOCKTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,
+    initial-scale=1">
+
+    <title>読書ログの登録</title>
+</head>
+
+<body>
+    <h1>読書ログ</h1>
+    <form action="create.php" method="POST"> 
+        <?php if (count($errors)) : ?>
+            <ul>
+                <?php foreach ($errors as $error) : ?>
+                    <li><?php echo $error; ?></li>
+                <?php endforeach; ?>    
+            </ul>
+        <?php endif; ?>
+
+        <div>
+            <label for="title">書籍名</label>
+            <input type="text" id="title" name="title">
+        </div>
+        <div>
+            <label for="author">著者名</label>
+            <input type="text" id="author" name="author">
+        </div>
+        <div>
+            <label>読書状況</label>
+            <div>
+                <label for="status1">未読</label>
+                <input type="radio" id="status" name="status1" value="未読">
+            </div>
+            <div>
+                <label for="status2">読んでる</label>
+                <input type="radio" id="status" name="status2" value="読んでいる">
+            </div>
+            <div>
+                <label for="status3">読了</label>
+                <input type="radio" id="status" name="status3" value="読了">
+            </div>
+        <div>
+            <label for="rate">評価(5点満点の整数)</label>
+            <input type="number" id="rate" name="rate" max="5" min="0">
+        </div>
+        <div>
+            <label for="review">感想</label>
+            <textarea type="text" id="review" name="review"></textarea>
+        </div>
+        <button type="submit">登録する</button>
+    </form>
+</body>
+</html>
