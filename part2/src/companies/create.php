b95a59d80a8f7ad12e724ce3c975834bd1530932
crypt0box <pray.happy76@gmail.com>
2021-01-15 20:28:02 +0900

    ADD: initial commit

--- part2/src/companies/create.php
+++ part2/src/companies/create.php
@@ -23,16 +23,92 @@ function createCompany($link, $company)
     }
 }
 
+function validate($company)
+{
+    $errors = [];
+
+    // 会社名
+    if (!strlen($company['name'])) {
+        $errors['name'] = '会社名を入力してください';
+    } elseif (strlen($company['name']) > 255) {
+        $errors['name'] = '会社名は255文字以内で入力してください';
+    }
+
+    // 設立日
+    $dates = explode('-', $company['establishment_date']);
+    if (!strlen($company['establishment_date'])) {
+        $errors['establishment_date'] = '設立日を入力してください';
+    } elseif (count($dates) !== 3) {
+        $errors['establishment_date'] = '設立日を正しい形式で入力してください';
+    } elseif (!checkdate($dates[1], $dates[2], $dates[0])) {
+        $errors['establishment_date'] = '設立日を正しい日付で入力してください';
+    }
+
+    // 代表者
+    if (!strlen($company['founder'])) {
+        $errors['founder'] = '代表者名を入力してください';
+    } elseif (strlen($company['founder']) > 100) {
+        $errors['founder'] = '代表者名は100文字以内で入力してください';
+    }
+
+    return $errors;
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $company = [
         'name' => $_POST['name'],
         'establishment_date' => $_POST['establishment_date'],
         'founder' => $_POST['founder']
     ];
-    // バリデーションする
-    $link = dbConnect();
-    createCompany($link, $company);
-    mysqli_close($link);
+
+    $errors = validate($company);
+    // バリデーションエラーがなければ
+    if (!count($errors)) {
+        $link = dbConnect();
+        createCompany($link, $company);
+        mysqli_close($link);
+        header("Location: index.php");
+    }
+    // もしエラーがあれば
 }
 
-header("Location: index.php");
+?>
+<!DOCKTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,
+    initial-scale=1">
+
+    <title>会社情報の登録</title>
+</head>
+
+<body>
+    <h1>会社情報の登録</h1>
+    <form action="create.php" method="POST">
+        <?php if (count($errors)) : ?>
+            <ul>
+                <?php foreach ($errors as $error) : ?>
+                    <li><?php echo $error; ?></li>
+                <?php endforeach; ?>    
+            </ul>
+        <?php endif; ?>
+
+        <div>
+            <label for="name">会社名</label>
+            <input type="text" id="name" name="name">
+        </div>
+        <div>
+            <label for="establishment_date">設立日</label>
+            <input type="date" name="establishment_date"
+            id="establishment_date">
+        </div>
+        <div>
+            <label for="founder">代表者</label>
+            <input type="text" name="founder" id="founder">
+        </div>
+        <button type="submit">登録する</button>
+    </form>
+</body>
+</html>
