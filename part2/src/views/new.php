e0154b5ea92a0ff3031e6f155722077bf80daf5c
crypt0box <pray.happy76@gmail.com>
2021-01-18 21:59:03 +0900

    ADD: initial commit

--- part2/src/views/new.php
+++ part2/src/views/new.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,
     initial-scale=1">
-
+    <link rel="stylesheet" href="stylesheets/css/app.css">
     <title>読書ログの登録</title>
 </head>
 
