ffc3828abe78bedf640beb5dc68fc9b028dff228
crypt0box <pray.happy76@gmail.com>
2021-01-22 19:53:58 +0900

    ADD: initial commit

--- part2/src/views/index.php
+++ part2/src/views/index.php
@@ -1,20 +1,21 @@
-<h1>読書ログの一覧</h1>
-<a href="new.php">読書ログを登録する</a>
+<a href="new.php" class="btn btn-primary mt-4 mb-4">読書ログを登録する</a>
 <main>
     <?php if (count($reviews) > 0) : ?>
         <?php foreach ($reviews as $review) : ?>
-            <section>
-                <h2>
-                    <?php echo escape($review['title']) ?>
-                </h2>
-                <div>
-                    <?php echo escape($review['author']) ?>&nbsp;/&nbsp;
-                    <?php echo escape($review['status']) ?>&nbsp;/&nbsp;
-                    <?php echo escape($review['rate']) ?>点
+            <section class="card shadow-sm mb-4">
+                <div class="card-body">
+                    <h2 class="card-title h4 mb-3">
+                        <?php echo escape($review['title']) ?>
+                    </h2>
+                    <div class="mb-3">
+                        <?php echo escape($review['author']) ?>&nbsp;/&nbsp;
+                        <?php echo escape($review['status']) ?>&nbsp;/&nbsp;
+                        <?php echo escape($review['rate']) ?>点
+                    </div>
+                    <p>
+                        <?php echo nl2br(escape($review['review']), false) ?>
+                    </p>
                 </div>
-                <p>
-                    <?php echo nl2br(escape($review['review']), false) ?>
-                </p>
             </section>
         <?php endforeach; ?>
     <?php else : ?>
