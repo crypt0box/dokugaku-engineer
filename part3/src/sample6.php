46928ef392b03f2a1e192b82ed8255ec1a1e2e3a
crypt0box <pray.happy76@gmail.com>
2021-02-02 20:55:35 +0900

    ADD: initial commit

---
+++ part3/src/sample6.php
@@ -0,0 +1,49 @@
+<?php
+
+if (0) {
+    echo 'true' . PHP_EOL;
+} else {
+    echo 'false' . PHP_EOL;
+} // false
+
+if (1) {
+    echo 'true' . PHP_EOL;
+} else {
+    echo 'false' . PHP_EOL;
+} // true
+
+if ("") {
+    echo 'true' . PHP_EOL;
+} else {
+    echo 'false' . PHP_EOL;
+} // false
+
+if ("a") {
+    echo 'true' . PHP_EOL;
+} else {
+    echo 'false' . PHP_EOL;
+} // true
+
+if ("0") {
+    echo 'true' . PHP_EOL;
+} else {
+    echo 'false' . PHP_EOL;
+} // false
+
+if ([]) {
+    echo 'true' . PHP_EOL;
+} else {
+    echo 'false' . PHP_EOL;
+} // false
+
+if ([0]) {
+    echo 'true' . PHP_EOL;
+} else {
+    echo 'false' . PHP_EOL;
+} // true
+
+if (null) {
+    echo 'true' . PHP_EOL;
+} else {
+    echo 'false' . PHP_EOL;
+} // false
