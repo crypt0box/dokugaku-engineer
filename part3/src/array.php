c35b0200b7952cf042d6d793bedda19dbfeb3ae1
crypt0box <pray.happy76@gmail.com>
2021-02-03 20:04:47 +0900

    ADD: initial commit

--- part3/src/array.php
+++ part3/src/array.php
@@ -19,3 +19,24 @@ $another_members = [
 ]; // 一部だけキーを付けたりできるが非推奨
 
 var_dump($another_members);
+
+$movie = [
+    'name' => 'totoro',
+];
+$movie['year'] = 1988;
+
+$movie2 = [
+    'director' => 'Miyazaki Hayao',
+];
+$movie3 = array_merge($movie, $movie2);
+
+var_dump($movie3);
+var_dump(count($movie3));
+
+$weights = [
+    'Kumagae' => 50,
+    'Suemitsu' => 90,
+    'Kawasaki' => 45,
+];
+asort($weights);
+var_dump($weights);
